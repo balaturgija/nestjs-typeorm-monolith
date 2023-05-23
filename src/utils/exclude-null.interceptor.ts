9bf84a4d34acfc995213ba1bfd004e9270e2553e
balaturgija <matej.balat@gmail.com>
2023-05-23 23:07:35 +0200

    Create exclude null interceptor

---
+++ src/utils/exclude-null.interceptor.ts
@@ -0,0 +1,17 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { recursivelyStripNullValues } from './recursively-skipp-null-values';
+
+@Injectable()
+export class ExcludeNullInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(map((value) => recursivelyStripNullValues(value)));
+  }
+}
